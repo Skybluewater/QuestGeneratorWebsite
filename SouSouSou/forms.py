7c0ec44a689349a08289c1261edb55822c35c9e4
Skybluewater <Skybluewater@users.noreply.github.com>
2019-12-18 14:08:19 +0800

    有了题目生成功能,从数据库中取出的数据filter后用output_list保存,需要打印

--- SouSouSou/forms.py
+++ SouSouSou/forms.py
@@ -1,29 +1,33 @@
 from django import forms
 
 
-class DSform(forms.ModelForm):
-    quantity = forms.IntegerField(label="要几个")
-
+class DSform(forms.Form):
+    quantity = forms.IntegerField(label="客官要几个题", min_value=0, max_value=50)
     optr = (
         ('2', "简单难度2"),
         ('3', "简单难度3"),
-        ('4', "4"),
-        ('5', "5"),
-        ('6', "6"),
-        ('7', "7"),
+        ('4', "简单难度4"),
+        ('5', "中等难度5"),
+        ('6', "中等难度6"),
+        ('7', "中等难度7"),
+        ('8', "小学生都哭了的难度8"),
+        ('9', "小学生都哭了的难度9"),
     )
-    operators = forms.ChoiceField(label="客官来几个符号", choices=optr)
+    operators = forms.MultipleChoiceField(label="客官来几个符号", choices=optr)
     if_pw = (
         ('True', "是"),
         ('False', "否"),
+        ('Both', "我都要"),
     )
     if_ng = (
         ('True', "是"),
         ('False', "否"),
+        ('Both', "我都要"),
     )
     if_fa = (
         ('True', "是"),
         ('False', "否"),
+        ('Both', "我都要"),
     )
     potyp = (
         ('True', "^"),
--- SouSouSou/views.py
+++ SouSouSou/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
 from .forms import DSform
 from question.models import Question
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 import random
 
-
-# Create your views here.
 
 def class_list(request):
     return render(request, 'SouSouSou/main.html')
@@ -23,24 +20,23 @@ def quest_generator(request):
     if request.method == 'POST':
         g_form = DSform(request.POST)
         if g_form.is_valid():
-            quantity = g_form.quantity
-            if_pow = g_form.if_pow
-            if_fra = g_form.if_fra
-            if_neg = g_form.if_neg
-            potp = g_form.potyp
-            operator = g_form.operators
-            quest: list
-            quest = Question.objects.filter(quantity=quantity, question_if_negative=if_neg,
-                                            question_operators_num=operator, question_if_pow=if_pow)
+            quantity = g_form.cleaned_data['quantity']
+            if_pow = g_form.cleaned_data['if_pow']
+            if_fra = g_form.cleaned_data['if_fra']
+            if_neg = g_form.cleaned_data['if_neg']
+            potp = g_form.cleaned_data['pow_type']
+            operator = g_form.cleaned_data['operators']
+            quest = get_ls(operator=operator, if_pow=if_pow, if_neg=if_neg)
             i = 0
-            output_list: list
+            print(quest)
+            ot_ls = []
             while i < quantity:
                 rand = random.randint(0, quest.__len__() - 1)
                 quest[rand].question = string_change(quest[rand].question, potp)
-                output_list.append(quest[rand])
+                ot_ls.append(quest[rand])
                 quest.pop(rand)
                 i += 1
-            context = {'output_list': output_list, 'if_fraction': if_fra, 'type': "POST"}
+            context = {'output_list': ot_ls, 'if_fraction': if_fra, 'type': "POST"}
             return render(request, 'SouSouSou/Generator.html', context)
         else:
             HttpResponse('form contents err')
@@ -53,3 +49,56 @@ def string_change(string: str, potp):
     if potp:
         return string.replace('^', '**')
     return string
+
+
+def get_ls(operator: list, if_pow, if_neg):
+    i = 0
+    rt_ls = []
+    if if_neg == 'both':
+        if if_pow == 'both':
+            while i < operator.__len__():
+                quest = Question.objects.filter(question_operators_num=operator[i])
+                i += 1
+                j = 0
+                while j < quest.__len__():
+                    rt_ls.append(quest[j])
+                    j += 1
+            return rt_ls
+        else:
+            while i < operator.__len__():
+                quest = Question.objects.filter(question_operators_num=operator[i], question_if_pow=if_pow)
+                i += 1
+                j = 0
+                while j < quest.__len__():
+                    rt_ls.append(quest[j])
+                    j += 1
+            return rt_ls
+    elif if_pow == 'both':
+        if if_neg == 'both':
+            while i < operator.__len__():
+                quest = Question.objects.filter(question_operators_num=operator[i])
+                i += 1
+                j = 0
+                while j < quest.__len__():
+                    rt_ls.append(quest[j])
+                    j += 1
+            return rt_ls
+        else:
+            while i < operator.__len__():
+                quest = Question.objects.filter(question_operators_num=operator[i], question_if_pow=if_pow)
+                i += 1
+                j = 0
+                while j < quest.__len__():
+                    rt_ls.append(quest[j])
+                    j += 1
+            return rt_ls
+    else:
+        while i < operator.__len__():
+            quest = Question.objects.filter(question_if_negative=if_neg,
+                                            question_operators_num=operator[i], question_if_pow=if_pow)
+            i += 1
+            j = 0
+            while j < quest.__len__():
+                rt_ls.append(quest[j])
+                j += 1
+        return rt_ls
